c5415dc3a6c7d7337824d47cea8235037560d513
이진영 <swlsdud1005@naver.com>
2023-09-12 13:20:40 +0900

    카카오 - 기본 골격, 스타일링 1차

--- src/app/kakao/page.tsx
+++ src/app/kakao/page.tsx
@@ -1,5 +1,10 @@
+import KakaoGetWebtoon from "@/components/KakaoGetWebtoon";
 import React from "react";
 
 export default function Kakao() {
-  return <div>카카오웹툰</div>;
+  return (
+    <>
+      <KakaoGetWebtoon />
+    </>
+  );
 }
