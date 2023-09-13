37faa8e358ffb1d987bf310ae56baf37bea4ef46
이진영 <swlsdud1005@naver.com>
2023-09-13 19:17:32 +0900

    웹툰페이지 - 데이터 불러오기 useQuery로 바꾸기

--- src/components/PageData.tsx
+++ src/components/PageData.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import React, { useEffect, useState } from "react";
-import { GetDataType, PageDataType } from "./TypeAlias";
+import React, { useState } from "react";
+import { PageDataType } from "./TypeAlias";
+import { useQuery } from "@tanstack/react-query";
 
 const DAY = [
   {
@@ -35,37 +36,17 @@ const DAY = [
 ];
 const CURRENT = new Date();
 const WEEK = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 export default function PageData({ page, name }: PageDataType) {
-  const MonData = require(`../../public/fakeData/${page}/mon1.json`);
-  const TueData = require(`../../public/fakeData/${page}/tue1.json`);
-  const WedData = require(`../../public/fakeData/${page}/wed1.json`);
-  const ThuData = require(`../../public/fakeData/${page}/thu1.json`);
-  const FriData = require(`../../public/fakeData/${page}/fri1.json`);
-  const SatData = require(`../../public/fakeData/${page}/sat1.json`);
-  const SunData = require(`../../public/fakeData/${page}/sun1.json`);
-
   const [currentDay, setCurrentDay] = useState<string>(
     `${WEEK[CURRENT.getDay()]}`
   );
-  const [getDataByDay, setGetDataByDay] = useState<GetDataType[]>([]);
-  const dayChange = (selected: string) => {
+  const dayChange = async (selected: string) => {
     setCurrentDay(selected);
-    if (selected === "mon") {
-      setGetDataByDay(MonData.webtoons);
-    } else if (selected === "tue") {
-      setGetDataByDay(TueData.webtoons);
-    } else if (selected === "wed") {
-      setGetDataByDay(WedData.webtoons);
-    } else if (selected === "thu") {
-      setGetDataByDay(ThuData.webtoons);
-    } else if (selected === "fri") {
-      setGetDataByDay(FriData.webtoons);
-    } else if (selected === "sat") {
-      setGetDataByDay(SatData.webtoons);
-    } else if (selected === "sun") {
-      setGetDataByDay(SunData.webtoons);
-    }
+    await fetch(
+      `${API_KEY}?perPage=20&page=1&service=${page}&updateDay=${selected}`
+    ).then((response) => response.json());
   };
 
   const goToPage = (url: string, name: string) => {
@@ -82,23 +63,12 @@ export default function PageData({ page, name }: PageDataType) {
     }
   };
 
-  useEffect(() => {
-    if (currentDay === "mon") {
-      setGetDataByDay(MonData.webtoons);
-    } else if (currentDay === "tue") {
-      setGetDataByDay(TueData.webtoons);
-    } else if (currentDay === "wed") {
-      setGetDataByDay(WedData.webtoons);
-    } else if (currentDay === "thu") {
-      setGetDataByDay(ThuData.webtoons);
-    } else if (currentDay === "fri") {
-      setGetDataByDay(FriData.webtoons);
-    } else if (currentDay === "sat") {
-      setGetDataByDay(SatData.webtoons);
-    } else if (currentDay === "sun") {
-      setGetDataByDay(SunData.webtoons);
-    }
-  }, []);
+  const { data } = useQuery(["data", page, currentDay], async () => {
+    const getData = await fetch(
+      `${API_KEY}?perPage=20&page=1&service=${page}&updateDay=${currentDay}`
+    ).then((response) => response.json());
+    return getData;
+  });
 
   return (
     <>
@@ -118,35 +88,36 @@ export default function PageData({ page, name }: PageDataType) {
         ))}
       </div>
       <div className="grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
-        {getDataByDay.map((card, key) => (
-          <div
-            className="shadow-lg rounded-lg h-[450px] lg:h-[350px]"
-            key={key}
-          >
-            <div className="w-full h-[80%] md:h-[70%]">
-              <img
-                className=" rounded-t-lg w-full h-full cursor-pointer"
-                src={card.img}
-                alt="card image"
-                onClick={() => {
-                  goToPage(card.url, name);
-                }}
-              />
-            </div>
-            <div className=" w-full h-[20%] md:h-[30%]">
-              <div className="w-full h-full flex items-center">
-                <h3
-                  className="pl-3 font-bold text-slate-700 truncate cursor-pointer"
+        {data &&
+          data.webtoons.map((card: any, key: any) => (
+            <div
+              className="shadow-lg rounded-lg h-[450px] lg:h-[350px]"
+              key={key}
+            >
+              <div className="w-full h-[80%] md:h-[70%]">
+                <img
+                  className=" rounded-t-lg w-full h-full cursor-pointer"
+                  src={card.img}
+                  alt="card image"
                   onClick={() => {
                     goToPage(card.url, name);
                   }}
-                >
-                  {card.title}
-                </h3>
+                />
+              </div>
+              <div className=" w-full h-[20%] md:h-[30%]">
+                <div className="w-full h-full flex items-center">
+                  <h3
+                    className="pl-3 font-bold text-slate-700 truncate cursor-pointer"
+                    onClick={() => {
+                      goToPage(card.url, name);
+                    }}
+                  >
+                    {card.title}
+                  </h3>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </>
   );
