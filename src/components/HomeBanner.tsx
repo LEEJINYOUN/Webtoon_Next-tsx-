359aa031335babf505caa4bfe8bbc35c2d3e9b23
이진영 <swlsdud1005@naver.com>
2023-09-13 17:24:45 +0900

    메인페이지 - 데이터 불러오기 useQuery로 바꾸기

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -1,29 +1,78 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import NaverData from "../../public/fakeData/homeBanner/naverWebtoon.json";
-import KakaoData from "../../public/fakeData/homeBanner/kakaoWebtoon.json";
-import KakaoPageData from "../../public/fakeData/homeBanner/kakaoPageWebtoon.json";
+import React from "react";
+import { useQueries } from "@tanstack/react-query";
 import BannerData from "./BannerData";
-import { GetDataType } from "./TypeAlias";
+
+const CURRENT = new Date();
+const WEEK = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+const PAGE = ["naver", "kakao", "kakaoPage"];
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 export default function HomeBanner() {
-  const [naverBanner, setNaverBanner] = useState<GetDataType[]>([]);
-  const [kakaoBanner, setKakaoBanner] = useState<GetDataType[]>([]);
-  const [kakaoPageBanner, setKakaoPageBanner] = useState<GetDataType[]>([]);
-  useEffect(() => {
-    setNaverBanner(NaverData.webtoons);
-    setKakaoBanner(KakaoData.webtoons);
-    setKakaoPageBanner(KakaoPageData.webtoons);
-  }, []);
+  const currentDay = WEEK[CURRENT.getDay()];
+  const bannerGetData = useQueries({
+    queries: [
+      {
+        queryKey: ["naver", PAGE[0], currentDay],
+        queryFn: async () => {
+          const getData = await fetch(
+            `${API_KEY}?perPage=5&page=1&service=${PAGE[0]}&updateDay=${currentDay}`
+          ).then((response) => response.json());
+          return getData;
+        },
+      },
+      {
+        queryKey: ["kakao", PAGE[1], currentDay],
+        queryFn: async () => {
+          const getData = await fetch(
+            `${API_KEY}?perPage=5&page=1&service=${PAGE[1]}&updateDay=${currentDay}`
+          ).then((response) => response.json());
+          return getData;
+        },
+      },
+      {
+        queryKey: ["kakaoPage", PAGE[2], currentDay],
+        queryFn: async () => {
+          const getData = await fetch(
+            `${API_KEY}?perPage=5&page=1&service=${PAGE[2]}&updateDay=${currentDay}`
+          ).then((response) => response.json());
+          return getData;
+        },
+      },
+    ],
+  });
+
   return (
     <>
-      <BannerData data={naverBanner} name={"네이버"} linkHref={"/naver"} />
-      <BannerData data={kakaoBanner} name={"카카오"} linkHref={"/kakao"} />
-      <BannerData
-        data={kakaoPageBanner}
-        name={"카카오페이지"}
-        linkHref={"/kakaoPage"}
-      />
+      {bannerGetData[0].data && (
+        <>
+          <BannerData
+            data={bannerGetData[0].data.webtoons}
+            name={"네이버"}
+            linkHref={"/naver"}
+          />
+        </>
+      )}
+
+      {bannerGetData[1].data && (
+        <>
+          <BannerData
+            data={bannerGetData[1].data.webtoons}
+            name={"카카오"}
+            linkHref={"/kakao"}
+          />
+        </>
+      )}
+
+      {bannerGetData[2].data && (
+        <>
+          <BannerData
+            data={bannerGetData[2].data.webtoons}
+            name={"카카이페이지"}
+            linkHref={"/kakaoPage"}
+          />
+        </>
+      )}
     </>
   );
 }
